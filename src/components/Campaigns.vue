3afe002480a2937c3a83e894c5d066e9f448cdb7
Alherd <algerd.blr@gmail.com>
2021-10-12 10:02:51 +0300

    Load campaigns

--- src/components/Campaigns.vue
+++ src/components/Campaigns.vue
@@ -45,318 +45,12 @@
     </div>
   </header-view>
   <div style="margin-top:190px" class="scene">
-    <div class="election-campaign-list">
-      <!-- Current campaigning -->
-      <div class="election-campaign-unit">
-        <div class="elect-camp-unit-header flex-row flex-algn-itms-c size-100 border-b-2px border-color2">
-          <div class="section flex-grow-all txt-size-20px pdng-20px pdng-l-40px pdng-r-40px">
-            <div class="txt-color-1 txt-medium">
-              6—9 августа 2021
-            </div>
-            <div class="txt-bold">
-              <div class="elect-camp-dot"></div>
-              Кампания уже начата, 2 дня до окончания
-            </div>
-          </div>
-          <div class="section flex-algn-slf-strch flex-row flex-algn-itms-c border-l-2px border-color2 pdng-20px pdng-l-40px pdng-r-40px cursor-pointer hovered">
-            <div class="flex-row flex-algn-itms-c">
-              <div class="section">
-                <svg width="25" height="33" viewBox="0 0 25 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M2.71436 2.57623C2.71436 2.57623 6.78428 0.261278 9.50878 0.0235027C13.0632 -0.286697 16.0314 2.57623 18.4774 2.57623C20.9234 2.57623 25.0001 0.874411 25.0001 0.874411V17.8926C25.0001 17.8926 22.2207 19.2549 20.3799 19.5944C16.1164 20.3807 13.8572 17.0417 9.50878 17.0417C5.16035 17.0417 2.71436 19.5944 2.71436 19.5944V2.57623Z" fill="#FF5C01"/>
-                  <rect y="1" width="2" height="32" fill="#FF5C01"/>
-                </svg>
-              </div>
-              <div class="section pdng-l-15px">
-                <div class="txt-size-20px">
-                  Нарушений
-                </div>
-                <div class="txt-size-20px txt-bold">
-                  1 337
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="elect-camp-unit-info pdng-t-30px pdng-b-50px pdng-l-40px pdng-r-40px">
-          <h2 class="txt-color-1 txt-size-48px txt-lh-1_1em">
-            <a class="txt-underline-2px" href="/campaign/1">Выбор президента Республики Беларусь 2020</a>
-          </h2>
-          <div class="tag-wrp mrgn-t-20px">
-            <a class="tag-unit">
-              Общереспубликанские выборы
-            </a>
-          </div>
-        </div>
-        <div class="elect-camp-unit-candidates border-t-2px border-color2">
-          <div class="elect-camp-unit-candds-wrp flex-row">
-            <div class="person-wrp flex-row flex-algn-itms-c cursor-pointer">
-              <div class="person-photo">
-                <div class="person-initials">С.Г.Т.</div>
-                <img src="/imgs/person-photo.png" alt="Светлана Георгиевна Тихановская">
-              </div>
-              <div class="person-info pdng-l-10px">
-                <div class="chart-wrp">
-                  <div class="chart-unit" style="width:67%;"></div>
-                  <div class="chart-value">
-                    67%
-                  </div>
-                </div>
-                <div class="person-name txt-size-14px txt-medium">
-                  Светлана Георгиевна
-                  <br>
-                  Тихановская
-                </div>
-                <div class="person-mark txt-color-3-1 txt-size-12px txt-medium">
-                  37 лет
-                </div>
-              </div>
-            </div>
-            <div class="person-wrp flex-row flex-algn-itms-c cursor-pointer">
-              <div class="person-photo">
-                <div class="person-initials">С.Г.Т.</div>
-                <img src="/imgs/person-photo.png" alt="Светлана Георгиевна Тихановская">
-              </div>
-              <div class="person-info pdng-l-10px">
-                <div class="chart-wrp">
-                  <div class="chart-unit" style="width:67%;"></div>
-                  <div class="chart-value">
-                    67%
-                  </div>
-                </div>
-                <div class="person-name txt-size-14px txt-medium">
-                  Светлана Георгиевна
-                  <br>
-                  Тихановская
-                </div>
-                <div class="person-mark txt-color-3-1 txt-size-12px txt-medium">
-                  37 лет
-                </div>
-                <!--<div class="infoblock txt-size-12px">
-        <div class="infoblock-name">
-          Должность
-        </div>
-        <div class="infoblock-value">
-          Президент Республики Беларусь
-        </div>
-        </div>-->
-              </div>
-            </div>
-            <div class="person-wrp flex-row flex-algn-itms-c cursor-pointer">
-              <div class="person-photo">
-                <div class="person-initials">С.Г.Т.</div>
-                <img src="/imgs/person-photo.png" alt="Светлана Георгиевна Тихановская">
-              </div>
-              <div class="person-info pdng-l-10px">
-                <div class="chart-wrp">
-                  <div class="chart-unit" style="width:67%;"></div>
-                  <div class="chart-value">
-                    67%
-                  </div>
-                </div>
-                <div class="person-name txt-size-14px txt-medium">
-                  Светлана Георгиевна
-                  <br>
-                  Тихановская
-                </div>
-                <div class="person-mark txt-color-3-1 txt-size-12px txt-medium">
-                  37 лет
-                </div>
-                <!--<div class="infoblock txt-size-12px">
-        <div class="infoblock-name">
-          Должность
-        </div>
-        <div class="infoblock-value">
-          Президент Республики Беларусь
-        </div>
-        </div>-->
-              </div>
-            </div>
-            <div class="person-wrp flex-row flex-algn-itms-c cursor-pointer">
-              <div class="person-photo">
-                <div class="person-initials">С.Г.Т.</div>
-                <img src="/imgs/person-photo.png" alt="Светлана Георгиевна Тихановская">
-              </div>
-              <div class="person-info pdng-l-10px">
-                <div class="chart-wrp">
-                  <div class="chart-unit" style="width:67%;"></div>
-                  <div class="chart-value">
-                    67%
-                  </div>
-                </div>
-                <div class="person-name txt-size-14px txt-medium">
-                  Светлана Георгиевна
-                  <br>
-                  Тихановская
-                </div>
-                <div class="person-mark txt-color-3-1 txt-size-12px txt-medium">
-                  37 лет
-                </div>
-                <!--<div class="infoblock txt-size-12px">
-        <div class="infoblock-name">
-          Должность
-        </div>
-        <div class="infoblock-value">
-          Президент Республики Беларусь
-        </div>
-        </div>-->
-              </div>
-            </div>
-            <div class="person-wrp flex-row flex-algn-itms-c cursor-pointer">
-              <div class="person-photo">
-                <div class="person-initials">С.Г.Т.</div>
-                <img src="/imgs/person-photo.png" alt="Светлана Георгиевна Тихановская">
-              </div>
-              <div class="person-info pdng-l-10px">
-                <div class="chart-wrp">
-                  <div class="chart-unit" style="width:67%;"></div>
-                  <div class="chart-value">
-                    67%
-                  </div>
-                </div>
-                <div class="person-name txt-size-14px txt-medium">
-                  Светлана Георгиевна
-                  <br>
-                  Тихановская
-                </div>
-                <div class="person-mark txt-color-3-1 txt-size-12px txt-medium">
-                  37 лет
-                </div>
-                <!--<div class="infoblock txt-size-12px">
-        <div class="infoblock-name">
-          Должность
-        </div>
-        <div class="infoblock-value">
-          Президент Республики Беларусь
-        </div>
-        </div>-->
-              </div>
-            </div>
-            <div class="person-wrp flex-row flex-algn-itms-c cursor-pointer">
-              <div class="person-photo">
-                <div class="person-initials">С.Г.Т.</div>
-                <img src="/imgs/person-photo.png" alt="Светлана Георгиевна Тихановская">
-              </div>
-              <div class="person-info pdng-l-10px">
-                <div class="chart-wrp">
-                  <div class="chart-unit" style="width:67%;"></div>
-                  <div class="chart-value">
-                    67%
-                  </div>
-                </div>
-                <div class="person-name txt-size-14px txt-medium">
-                  Светлана Георгиевна
-                  <br>
-                  Тихановская
-                </div>
-                <div class="person-mark txt-color-3-1 txt-size-12px txt-medium">
-                  37 лет
-                </div>
-                <!--<div class="infoblock txt-size-12px">
-        <div class="infoblock-name">
-          Должность
-        </div>
-        <div class="infoblock-value">
-          Президент Республики Беларусь
-        </div>
-        </div>-->
-              </div>
-            </div>
-            <div class="person-wrp flex-row flex-algn-itms-c cursor-pointer">
-              <div class="person-photo">
-                <div class="person-initials">С.Г.Т.</div>
-                <img src="/imgs/person-photo.png" alt="Светлана Георгиевна Тихановская">
-              </div>
-              <div class="person-info pdng-l-10px">
-                <div class="chart-wrp">
-                  <div class="chart-unit" style="width:67%;"></div>
-                  <div class="chart-value">
-                    67%
-                  </div>
-                </div>
-                <div class="person-name txt-size-14px txt-medium">
-                  Светлана Георгиевна
-                  <br>
-                  Тихановская
-                </div>
-                <div class="person-mark txt-color-3-1 txt-size-12px txt-medium">
-                  37 лет
-                </div>
-                <!--<div class="infoblock txt-size-12px">
-        <div class="infoblock-name">
-          Должность
-        </div>
-        <div class="infoblock-value">
-          Президент Республики Беларусь
-        </div>
-        </div>-->
-              </div>
-            </div>
-          </div>
-          <div class="elect-camp-unit-candds-mask">
-            <div class="button primary flex-row flex-algn-itms-c">
-              <div class="section">
-                Все кандидаты
-              </div>
-              <div class="section mrgn-l-10px">
-                <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M1 5.25C0.585786 5.25 0.25 5.58579 0.25 6C0.25 6.41421 0.585786 6.75 1 6.75V5.25ZM13.5303 6.53033C13.8232 6.23744 13.8232 5.76256 13.5303 5.46967L8.75736 0.696699C8.46447 0.403806 7.98959 0.403806 7.6967 0.696699C7.40381 0.989593 7.40381 1.46447 7.6967 1.75736L11.9393 6L7.6967 10.2426C7.40381 10.5355 7.40381 11.0104 7.6967 11.3033C7.98959 11.5962 8.46447 11.5962 8.75736 11.3033L13.5303 6.53033ZM1 6.75H13V5.25H1V6.75Z" fill="white"/>
-                </svg>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Upcoming secondary campaigning -->
-      <div class="election-campaign-unit">
-        <div class="elect-camp-unit-header flex-row flex-algn-itms-c size-100 border-b-2px border-color2">
-          <div class="section flex-grow-all txt-size-18px pdng-20px pdng-l-40px pdng-r-40px">
-            <div class="txt-color-1 txt-medium">
-              11—15 сентября 2021
-            </div>
-            <div class="txt-bold">
-              23 дня до начала
-            </div>
-          </div>
-          <div class="section border-l-2px border-color2 pdng-20px pdng-l-40px pdng-r-40px cursor-pointer hovered">
-            <div class="flex-row flex-algn-itms-c">
-              <div class="section">
-                <svg width="25" height="33" viewBox="0 0 25 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M2.71436 2.57623C2.71436 2.57623 6.78428 0.261278 9.50878 0.0235027C13.0632 -0.286697 16.0314 2.57623 18.4774 2.57623C20.9234 2.57623 25.0001 0.874411 25.0001 0.874411V17.8926C25.0001 17.8926 22.2207 19.2549 20.3799 19.5944C16.1164 20.3807 13.8572 17.0417 9.50878 17.0417C5.16035 17.0417 2.71436 19.5944 2.71436 19.5944V2.57623Z" fill="#FF5C01"/>
-                  <rect y="1" width="2" height="32" fill="#FF5C01"/>
-                </svg>
-              </div>
-              <div class="section pdng-l-15px">
-                <div class="txt-size-18px">
-                  Нарушений
-                </div>
-                <div class="txt-size-18px txt-bold">
-                  1 337
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="elect-camp-unit-info pdng-t-20px pdng-b-40px pdng-l-40px pdng-r-40px">
-          <h2 class="txt-color-1 txt-size-36px txt-lh-1_1em">
-            <a class="txt-underline-inline" href="/campaign/1">
-              Выборы представителей нижней палаты верхнего представительства третьего созыва депутатов
-            </a>
-          </h2>
-          <div class="tag-wrp mrgn-t-20px">
-            <a class="tag-unit">
-              Общереспубликанские выборы
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <!-- Archive campaigning -->
-      <div class="election-campaign-unit">
+    <div class="election-campaign-list" v-if="data">
+      <div class="election-campaign-unit" v-for="campaign of data.campaigns">
         <div class="elect-camp-unit-header flex-row flex-algn-itms-c size-100 border-b-2px border-color2">
           <div class="section flex-grow-all txt-size-18px pdng-20px pdng-l-40px pdng-r-40px grayscale">
             <div class="txt-color-1 txt-medium">
-              6—9 августа 2021
+              {{ campaign.started_at }} - {{ campaign.ended_at }}
             </div>
             <div class="txt-bold">
               Архив
@@ -366,7 +60,9 @@
             <div class="flex-row flex-algn-itms-c">
               <div class="section">
                 <svg width="25" height="33" viewBox="0 0 25 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M2.71436 2.57623C2.71436 2.57623 6.78428 0.261278 9.50878 0.0235027C13.0632 -0.286697 16.0314 2.57623 18.4774 2.57623C20.9234 2.57623 25.0001 0.874411 25.0001 0.874411V17.8926C25.0001 17.8926 22.2207 19.2549 20.3799 19.5944C16.1164 20.3807 13.8572 17.0417 9.50878 17.0417C5.16035 17.0417 2.71436 19.5944 2.71436 19.5944V2.57623Z" fill="#FF5C01"/>
+                  <path
+                      d="M2.71436 2.57623C2.71436 2.57623 6.78428 0.261278 9.50878 0.0235027C13.0632 -0.286697 16.0314 2.57623 18.4774 2.57623C20.9234 2.57623 25.0001 0.874411 25.0001 0.874411V17.8926C25.0001 17.8926 22.2207 19.2549 20.3799 19.5944C16.1164 20.3807 13.8572 17.0417 9.50878 17.0417C5.16035 17.0417 2.71436 19.5944 2.71436 19.5944V2.57623Z"
+                      fill="#FF5C01"/>
                   <rect y="1" width="2" height="32" fill="#FF5C01"/>
                 </svg>
               </div>
@@ -375,7 +71,7 @@
                   Нарушений
                 </div>
                 <div class="txt-size-18px txt-bold">
-                  1 337
+                  {{ campaign.messages.aggregate.count }}
                 </div>
               </div>
             </div>
@@ -383,15 +79,10 @@
         </div>
         <div class="elect-camp-unit-info pdng-t-20px pdng-b-40px pdng-l-40px pdng-r-40px grayscale">
           <h2 class="txt-color-1 txt-size-36px txt-lh-1_1em">
-            <a class="txt-underline-inline" href="/campaign/1">
-              Выборы представителей нижней палаты верхнего представительства третьего созыва депутатов
+            <a class="txt-underline-inline" :href="'/campaign/' + campaign.id">
+              {{ campaign.name }}
             </a>
           </h2>
-          <div class="tag-wrp mrgn-t-20px">
-            <a class="tag-unit">
-              Общереспубликанские выборы
-            </a>
-          </div>
         </div>
       </div>
 
@@ -404,12 +95,36 @@
   </div>
 </template>
 <script>
-import Header from './Header.vue';
-import {defineComponent} from "vue";
+import Header from './Header.vue'
+import {defineComponent, ref} from "vue";
 
+function fetchCampaigns() {
+  const data = ref(null)
+  const error = ref(null)
+  const load = async () => {
+    try {
+      const response = await fetch(import.meta.env.VITE_API_URL + '/campaigns')
+      data.value = await response.json()
+    } catch (e) {
+      error.value = e.message;
+      console.error(e)
+    }
+  }
+
+  return {data, load, error}
+}
+
+export {fetchCampaigns}
 export default defineComponent({
   components: {
     'header-view': Header
+  },
+  setup() {
+    const {data, load} = fetchCampaigns();
+    load()
+    return {
+      data
+    }
   }
 })
 </script>
