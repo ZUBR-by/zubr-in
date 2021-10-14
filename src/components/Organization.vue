3b958487a6f9af989e1706917eb7d19d63fbf9bf
Alherd <algerd.blr@gmail.com>
2021-10-14 20:32:31 +0300

    Отображение выдвинут членов от организаций по выборам

--- src/components/Organization.vue
+++ src/components/Organization.vue
@@ -76,15 +76,17 @@
     <h2 class="flex-row flex-algn-itms-c txt-size-36px txt-bold pdng-b-40px">
       Члены комиссий от организации
       <div class="buttongroup mrgn-l-20px">
-        <div class="buttongroup-unit active">
-          Списком
-        </div>
-        <div class="buttongroup-unit">
-          По выборам
+        <div class="buttongroup">
+          <button class="buttongroup-unit" :class="{active: view === 'list'}" @click="view = 'list'">
+            Списком
+          </button>
+          <button class="buttongroup-unit" :class="{active: view === 'group'}" @click="view = 'group'">
+            По выборам
+          </button>
         </div>
       </div>
     </h2>
-    <div>
+    <div v-if="view === 'list'">
       <div class="campaign-candidates-list">
         <div class="person-wrp flex-column flex-algn-itms-strch" v-for="item of data.organization.persons">
           <div class="person-photo">
@@ -166,6 +168,84 @@
         </div>
       </div>
     </div>
+    <div v-if="view === 'group'">
+      <div class="election-campaign-list">
+        <div class="election-campaign-unit">
+          <div class="elect-camp-unit-header flex-row flex-algn-itms-c size-100 border-b-2px border-color2">
+            <div class="section flex-grow-all txt-size-18px pdng-20px pdng-l-40px pdng-r-40px grayscale">
+              <div class="txt-color-1 txt-medium">
+                6—9 августа 2021
+              </div>
+              <div class="txt-bold">
+                Архив
+              </div>
+            </div>
+            <div class="section border-l-2px border-color2 pdng-20px pdng-l-40px pdng-r-40px cursor-pointer hovered">
+              <div class="flex-row flex-algn-itms-c">
+                <div class="section">
+                  <svg width="25" height="33" viewBox="0 0 25 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M2.71436 2.57623C2.71436 2.57623 6.78428 0.261278 9.50878 0.0235027C13.0632 -0.286697 16.0314 2.57623 18.4774 2.57623C20.9234 2.57623 25.0001 0.874411 25.0001 0.874411V17.8926C25.0001 17.8926 22.2207 19.2549 20.3799 19.5944C16.1164 20.3807 13.8572 17.0417 9.50878 17.0417C5.16035 17.0417 2.71436 19.5944 2.71436 19.5944V2.57623Z" fill="#FF5C01"/>
+                    <rect y="1" width="2" height="32" fill="#FF5C01"/>
+                  </svg>
+                </div>
+                <div class="section pdng-l-15px">
+                  <div class="txt-size-18px">
+                    Нарушений
+                  </div>
+                  <div class="txt-size-18px txt-bold">
+                    1 337
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="elect-camp-unit-info pdng-t-20px pdng-b-40px pdng-l-40px pdng-r-40px grayscale">
+            <h2 class="txt-color-1 txt-size-36px txt-lh-1_1em">
+              <a class="txt-underline-inline" href="#">
+                Выборы представителей нижней палаты верхнего представительства третьего созыва депутатов
+              </a>
+            </h2>
+            <div class="tag-wrp mrgn-t-20px">
+              <a class="tag-unit">
+                Общереспубликанские выборы
+              </a>
+            </div>
+          </div>
+          <div class="pdng-10px border-t-1px border-color1">
+            <div class="campaign-candidates-list size-100 overflow-hide">
+              <div class="person-wrp flex-column flex-algn-itms-strch cursor-pointer" v-for="i in [1,2,3]">
+                <div class="person-photo">
+                  <div class="person-initials">С.Г.Т.</div>
+                  <img src="/img/user.svg" alt="Светлана Георгиевна Тихановская">
+                </div>
+                <div class="person-info pdng-t-10px">
+                  <div class="person-name txt-size-14px txt-medium">
+                    Мазарчук Дмитрий
+                    <br>
+                    Валерьевич
+                  </div>
+                  <div class="person-mark txt-color-2 txt-size-12px">
+                    <div class="flex-row flex-algn-itms-c mrgn-t-5px">
+                      <div class="section">
+                        <img src="/img/icon/flag.svg">
+                      </div>
+                      <div class="section pdng-l-5px">
+                        <div class="txt-bold">
+                          237
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="txt-color-1 txt-size-12px txt-medium mrgn-t-10px">
+                    Председатель комиссии
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -196,8 +276,10 @@ export default defineComponent({
     onMounted(() => {
       fetchOrganization()
     })
+    const view = ref('list')
     return {
-      data
+      data,
+      view
     }
   }
 })
