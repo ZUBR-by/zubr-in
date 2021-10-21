14357c351eecd22e3573f201fedb4c2ab4a5b1e6
Alherd <algerd.blr@gmail.com>
2021-10-21 11:09:41 +0300

    Css loading priority

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from "./router";
 import 'primevue/resources/themes/saga-orange/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
+import './assets/main.css'
 import PrimeVue from 'primevue/config';
+
 const app = createApp(App)
 app.use(router)
 app.use(PrimeVue);
