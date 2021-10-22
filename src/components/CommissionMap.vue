ac568fbb82347d3e61a380cc6f99d8f196274935
Alherd <algerd.blr@gmail.com>
2021-10-22 12:18:16 +0300

    Первая версия карты комиссий

---
+++ src/components/CommissionMap.vue
@@ -0,0 +1,395 @@
+<template>
+  <div id="map" style="height: 100%;width: 100%"></div>
+</template>
+<script>
+import Map          from "ol/Map";
+import TileLayer    from "ol/layer/Tile";
+import {Cluster, OSM}          from "ol/source";
+import View         from "ol/View";
+import {fromLonLat} from "ol/proj";
+import VectorLayer  from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import {Feature}    from "ol";
+import Point        from "ol/geom/Point";
+import { Circle, Fill, Icon, Style, Text } from 'ol/style';
+
+export default {
+  props: {
+    feature  : Object
+  },
+  mounted() {
+    document.getElementById('map').innerHTML = '';
+    let map                                  = new Map({
+      layers      : [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target      : 'map',
+      view        : new View({
+        zoom  : 6.8,
+        center: fromLonLat([
+          27.7834,
+          53.7098
+        ]),
+      })
+    });
+
+    const cluster = new Cluster({
+      distance: 40,
+      source:  new VectorSource({
+        features: [
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                27.6308768,
+                53.8753372
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                27.5696487,
+                53.8753159
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                30.053611,
+                53.0825
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                24.741917,
+                53.6047649
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                27.5300215,
+                53.8942681
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                23.680605,
+                52.086457
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                23.681561,
+                52.086983
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                23.678119,
+                52.10569
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                23.680651,
+                52.086378
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                23.70575,
+                52.088785
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                26.011186,
+                53.145133
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                24.979748,
+                52.534316
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                26.424223,
+                52.761166
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                25.163872,
+                52.190339
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                24.029503,
+                52.199772
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                25.533363,
+                52.145513
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                25.341007,
+                52.71709
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                23.819638,
+                52.39655
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                24.356678,
+                52.208454
+              ]
+            }
+          },
+          {
+            "coordinates": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "urn:ogc:def:crs:EPSG::4326"
+                }
+              },
+              "coordinates": [
+                26.265859,
+                53.039146
+              ]
+            }
+          }
+        ].map(i => new Feature({
+          geometry: new Point(fromLonLat([
+            ...i.coordinates.coordinates
+          ])) ,
+        })),
+      }),
+    });
+    const styleCache = {};
+    let marker                               = new VectorLayer({
+      source: cluster,
+      style(feature) {
+
+        const size = feature.get('features').length;
+        let style = styleCache[size];
+
+        if (!style) {
+
+          style = new Style({
+            image: new Icon({
+              src: '/img/marker.png',
+              scale: 0.3,
+            }),
+          });
+
+          if (size > 1) {
+            style = [new Style({
+              image: new Circle({
+                radius: 20,
+                fill: new Fill({
+                  color: 'rgb(250,127,37, 0.65)',
+                }),
+              }),
+              text: new Text({
+                text: size.toString(),
+                font: '14px sans-serif',
+                fill: new Fill({
+                  color: '#fff',
+                }),
+              }),
+            })];
+          }
+
+          styleCache[size] = style;
+
+        }
+
+        return style;
+      },
+    });
+
+    map.addLayer(marker);
+  }
+}
+</script>
--- src/components/Commissions.vue
+++ src/components/Commissions.vue
@@ -46,7 +46,7 @@
             <button class="buttongroup-unit" :class="{active: view === 'list'}" @click="view = 'list'">
               Список
             </button>
-            <button class="buttongroup-unit" :class="{active: view === 'map'}" @click="view = 'map'">
+            <button class="buttongroup-unit" :class="{active: view === 'map'}" @click="view = 'map';mapInit = true">
               Карта
             </button>
           </div>
@@ -67,7 +67,8 @@
               {{ item.description }}
             </div>
           </div>
-          <div class="section pdng-r-20px pdng-l-20px pdng-t-20px pdng-b-20px border-l-1px border-color2 mil-border-0 mil-size-100 mil-pdng-15px mil-pdng-t-0 mil-border-b-1px mil-border-color1">
+          <div
+              class="section pdng-r-20px pdng-l-20px pdng-t-20px pdng-b-20px border-l-1px border-color2 mil-border-0 mil-size-100 mil-pdng-15px mil-pdng-t-0 mil-border-b-1px mil-border-color1">
             <div class="txt-color-1 txt-size-20px txt-medium mil-txt-size-16px">
               {{ item.code }}
             </div>
@@ -92,8 +93,11 @@
               <div class="section pdng-l-20px" v-if="item.observers_count">
                 <div class="flex-row flex-algn-itms-c">
                   <div class="section">
-                    <svg class="block" width="22" height="13" viewBox="0 0 22 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M11 0.75C15.0486 0.75 17.6168 2.28517 19.1763 3.78975C19.9622 4.54792 20.4986 5.30548 20.8368 5.86991C20.9913 6.12764 21.1037 6.34396 21.1792 6.5C21.1037 6.65604 20.9913 6.87235 20.8368 7.13009C20.4986 7.69452 19.9622 8.45208 19.1763 9.21025C17.6168 10.7148 15.0486 12.25 11 12.25C6.95137 12.25 4.38316 10.7148 2.82367 9.21025C2.03783 8.45208 1.50143 7.69452 1.16317 7.13009C1.00871 6.87235 0.89626 6.65604 0.820791 6.5C0.89626 6.34396 1.00871 6.12764 1.16317 5.86991C1.50143 5.30548 2.03783 4.54792 2.82367 3.78975C4.38316 2.28517 6.95137 0.75 11 0.75ZM0.700307 6.23154C0.700114 6.23104 0.70007 6.23093 0.700174 6.2312L0.700307 6.23154Z" stroke="#FF5C01" stroke-width="1.5"/>
+                    <svg class="block" width="22" height="13" viewBox="0 0 22 13" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                      <path
+                          d="M11 0.75C15.0486 0.75 17.6168 2.28517 19.1763 3.78975C19.9622 4.54792 20.4986 5.30548 20.8368 5.86991C20.9913 6.12764 21.1037 6.34396 21.1792 6.5C21.1037 6.65604 20.9913 6.87235 20.8368 7.13009C20.4986 7.69452 19.9622 8.45208 19.1763 9.21025C17.6168 10.7148 15.0486 12.25 11 12.25C6.95137 12.25 4.38316 10.7148 2.82367 9.21025C2.03783 8.45208 1.50143 7.69452 1.16317 7.13009C1.00871 6.87235 0.89626 6.65604 0.820791 6.5C0.89626 6.34396 1.00871 6.12764 1.16317 5.86991C1.50143 5.30548 2.03783 4.54792 2.82367 3.78975C4.38316 2.28517 6.95137 0.75 11 0.75ZM0.700307 6.23154C0.700114 6.23104 0.70007 6.23093 0.700174 6.2312L0.700307 6.23154Z"
+                          stroke="#FF5C01" stroke-width="1.5"/>
                       <circle cx="11" cy="6" r="3" stroke="#FF5C01" stroke-width="2"/>
                     </svg>
                   </div>
@@ -110,104 +114,34 @@
       </div>
       <div class="flex-column flex-algn-itms-c pdng-t-40px">
         <a href="#" class="button primary pdng-l-40px pdng-r-40px">
-          Загрузить еще 100 избирательных комиссий из {{data.pagination.aggregate.count}}
+          Загрузить еще 100 избирательных комиссий из {{ data.pagination.aggregate.count }}
         </a>
       </div>
     </template>
   </div>
-  <div style="margin-top:170px; max-width:100%; padding:0;" class="scene" v-if="view === 'map'">
-    <div class="map-wrp" style="background:#EDEDED; width:auto; min-height:640px">
-      <div class="map-wrp-legend txt-size-14px">
-        <div class="inline-flex flex-algn-itms-c txt-color-3-1">
-          <div>
-            <svg class="block" width="22" height="14" viewBox="0 0 22 14" fill="none"
-                 xmlns="http://www.w3.org/2000/svg">
-              <path
-                  d="M11 1.25C15.0486 1.25 17.6168 2.78517 19.1763 4.28975C19.9622 5.04792 20.4986 5.80548 20.8368 6.36991C20.9913 6.62764 21.1037 6.84396 21.1792 7C21.1037 7.15604 20.9913 7.37235 20.8368 7.63009C20.4986 8.19452 19.9622 8.95208 19.1763 9.71025C17.6168 11.2148 15.0486 12.75 11 12.75C6.95137 12.75 4.38316 11.2148 2.82367 9.71025C2.03783 8.95208 1.50143 8.19452 1.16317 7.63009C1.00871 7.37235 0.89626 7.15604 0.820791 7C0.89626 6.84396 1.00871 6.62764 1.16317 6.36991C1.50143 5.80548 2.03783 5.04792 2.82367 4.28975C4.38316 2.78517 6.95137 1.25 11 1.25ZM0.700307 6.73154C0.700114 6.73104 0.70007 6.73093 0.700174 6.7312L0.700307 6.73154Z"
-                  stroke="#FF5C01" stroke-width="1.5"></path>
-              <circle cx="11" cy="6.5" r="3" stroke="#FF5C01" stroke-width="2"></circle>
-            </svg>
-          </div>
-          <div class="mrgn-l-5px">
-            Количество наблюдателей
-          </div>
-        </div>
-        <div class="inline-flex flex-algn-itms-c txt-color-3-1 mrgn-l-20px">
-          <div>
-            <svg class="block" width="14" height="16" viewBox="0 0 14 16" fill="none"
-                 xmlns="http://www.w3.org/2000/svg">
-              <path
-                  d="M1.51855 1.94125C1.51855 1.94125 3.79543 0.646169 5.31963 0.513148C7.30809 0.33961 8.96866 1.94125 10.3371 1.94125C11.7054 1.94125 13.9861 0.989181 13.9861 0.989181V10.5098C13.9861 10.5098 12.4312 11.272 11.4014 11.4619C9.01622 11.9018 7.75232 10.0338 5.31963 10.0338C2.88694 10.0338 1.51855 11.4619 1.51855 11.4619V1.94125Z"
-                  fill="#FF5C01"></path>
-              <rect y="0.5" width="1.15385" height="15" fill="#FF5C01"></rect>
-            </svg>
-          </div>
-          <div class="mrgn-l-5px">
-            Количество инцидентов
-          </div>
-        </div>
-      </div>
+  <div v-show="view === 'map'" v-if="mapInit">
+    <div style="max-width:100%; padding:0;" class="scene mil-mrgn-t-120px">
+      <!--      <div class="map-wrp" style="background:#EDEDED; width:auto; min-height:640px">-->
 
-      <a class="map-mark" style="top:170px; left:160px;">
-        <div class="section pdng-8px">
-          <div class="flex-row flex-algn-itms-c">
-            <div class="section">
+      <div class="map-wrp" style="background:#EDEDED; min-height:640px;height: 300px">
+        <commission-map v-if="mapInit === true"></commission-map>
+        <div class="map-wrp-legend txt-size-14px mil-flex-row mil-notdisplay">
+          <div class="inline-flex flex-algn-itms-c txt-color-3-1 mil-size-50">
+            <div>
               <svg class="block" width="22" height="14" viewBox="0 0 22 14" fill="none"
                    xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M11 1.25C15.0486 1.25 17.6168 2.78517 19.1763 4.28975C19.9622 5.04792 20.4986 5.80548 20.8368 6.36991C20.9913 6.62764 21.1037 6.84396 21.1792 7C21.1037 7.15604 20.9913 7.37235 20.8368 7.63009C20.4986 8.19452 19.9622 8.95208 19.1763 9.71025C17.6168 11.2148 15.0486 12.75 11 12.75C6.95137 12.75 4.38316 11.2148 2.82367 9.71025C2.03783 8.95208 1.50143 8.19452 1.16317 7.63009C1.00871 7.37235 0.89626 7.15604 0.820791 7C0.89626 6.84396 1.00871 6.62764 1.16317 6.36991C1.50143 5.80548 2.03783 5.04792 2.82367 4.28975C4.38316 2.78517 6.95137 1.25 11 1.25ZM0.700307 6.73154C0.700114 6.73104 0.70007 6.73093 0.700174 6.7312L0.700307 6.73154Z"
-                    stroke="#FF5C01" stroke-width="1.5"/>
-                <circle cx="11" cy="6.5" r="3" stroke="#FF5C01" stroke-width="2"/>
+                    stroke="#FF5C01" stroke-width="1.5"></path>
+                <circle cx="11" cy="6.5" r="3" stroke="#FF5C01" stroke-width="2"></circle>
               </svg>
             </div>
-            <div class="section pdng-l-5px">
-              <div class=" txt-color-2 txt-size-12px txt-bold">
-                120
-              </div>
+            <div class="mrgn-l-5px">
+              Количество наблюдателей
             </div>
           </div>
-        </div>
-        <div class="section pdng-8px border-l-1px border-color2">
-          <div class="flex-row flex-algn-itms-c">
-            <div class="section">
-              <svg class="block" width="14" height="16" viewBox="0 0 14 16" fill="none"
-                   xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M1.51855 1.94125C1.51855 1.94125 3.79543 0.646169 5.31963 0.513148C7.30809 0.33961 8.96866 1.94125 10.3371 1.94125C11.7054 1.94125 13.9861 0.989181 13.9861 0.989181V10.5098C13.9861 10.5098 12.4312 11.272 11.4014 11.4619C9.01622 11.9018 7.75232 10.0338 5.31963 10.0338C2.88694 10.0338 1.51855 11.4619 1.51855 11.4619V1.94125Z"
-                    fill="#FF5C01"/>
-                <rect y="0.5" width="1.15385" height="15" fill="#FF5C01"/>
-              </svg>
-            </div>
-            <div class="section pdng-l-5px">
-              <div class=" txt-color-2 txt-size-12px txt-bold">
-                1 337
-              </div>
-            </div>
-          </div>
-        </div>
-      </a>
-      <a class="map-mark mark" style="top:220px; left:220px;">
-        <div class="section pdng-8px">
-          <div class="flex-row flex-algn-itms-c">
-            <div class="section">
-              <svg class="block" width="22" height="14" viewBox="0 0 22 14" fill="none"
-                   xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M11 1.25C15.0486 1.25 17.6168 2.78517 19.1763 4.28975C19.9622 5.04792 20.4986 5.80548 20.8368 6.36991C20.9913 6.62764 21.1037 6.84396 21.1792 7C21.1037 7.15604 20.9913 7.37235 20.8368 7.63009C20.4986 8.19452 19.9622 8.95208 19.1763 9.71025C17.6168 11.2148 15.0486 12.75 11 12.75C6.95137 12.75 4.38316 11.2148 2.82367 9.71025C2.03783 8.95208 1.50143 8.19452 1.16317 7.63009C1.00871 7.37235 0.89626 7.15604 0.820791 7C0.89626 6.84396 1.00871 6.62764 1.16317 6.36991C1.50143 5.80548 2.03783 5.04792 2.82367 4.28975C4.38316 2.78517 6.95137 1.25 11 1.25ZM0.700307 6.73154C0.700114 6.73104 0.70007 6.73093 0.700174 6.7312L0.700307 6.73154Z"
-                    stroke="#FF5C01" stroke-width="1.5"/>
-                <circle cx="11" cy="6.5" r="3" stroke="#FF5C01" stroke-width="2"/>
-              </svg>
-            </div>
-            <div class="section pdng-l-5px">
-              <div class=" txt-color-1 txt-size-12px txt-bold">
-                0
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="section pdng-8px border-l-1px border-color2">
-          <div class="flex-row flex-algn-itms-c">
-            <div class="section">
+          <div class="inline-flex flex-algn-itms-c txt-color-3-1 mrgn-l-20px">
+            <div>
               <svg class="block" width="14" height="16" viewBox="0 0 14 16" fill="none"
                    xmlns="http://www.w3.org/2000/svg">
                 <path
@@ -216,20 +150,98 @@
                 <rect y="0.5" width="1.15385" height="15" fill="#FF5C01"></rect>
               </svg>
             </div>
-            <div class="section pdng-l-5px">
-              <div class=" txt-color-2 txt-size-12px txt-bold">
-                0
-              </div>
+            <div class="mrgn-l-5px">
+              Количество инцидентов
             </div>
           </div>
         </div>
-      </a>
 
+
+        <a class="map-mark" style="top:170px; left:160px;">
+          <div class="section pdng-8px">
+            <div class="flex-row flex-algn-itms-c">
+              <div class="section">
+                <svg class="block" width="22" height="14" viewBox="0 0 22 14" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path
+                      d="M11 1.25C15.0486 1.25 17.6168 2.78517 19.1763 4.28975C19.9622 5.04792 20.4986 5.80548 20.8368 6.36991C20.9913 6.62764 21.1037 6.84396 21.1792 7C21.1037 7.15604 20.9913 7.37235 20.8368 7.63009C20.4986 8.19452 19.9622 8.95208 19.1763 9.71025C17.6168 11.2148 15.0486 12.75 11 12.75C6.95137 12.75 4.38316 11.2148 2.82367 9.71025C2.03783 8.95208 1.50143 8.19452 1.16317 7.63009C1.00871 7.37235 0.89626 7.15604 0.820791 7C0.89626 6.84396 1.00871 6.62764 1.16317 6.36991C1.50143 5.80548 2.03783 5.04792 2.82367 4.28975C4.38316 2.78517 6.95137 1.25 11 1.25ZM0.700307 6.73154C0.700114 6.73104 0.70007 6.73093 0.700174 6.7312L0.700307 6.73154Z"
+                      stroke="#FF5C01" stroke-width="1.5"/>
+                  <circle cx="11" cy="6.5" r="3" stroke="#FF5C01" stroke-width="2"/>
+                </svg>
+              </div>
+              <div class="section pdng-l-5px">
+                <div class=" txt-color-2 txt-size-12px txt-bold">
+                  120
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="section pdng-8px border-l-1px border-color2">
+            <div class="flex-row flex-algn-itms-c">
+              <div class="section">
+                <svg class="block" width="14" height="16" viewBox="0 0 14 16" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path
+                      d="M1.51855 1.94125C1.51855 1.94125 3.79543 0.646169 5.31963 0.513148C7.30809 0.33961 8.96866 1.94125 10.3371 1.94125C11.7054 1.94125 13.9861 0.989181 13.9861 0.989181V10.5098C13.9861 10.5098 12.4312 11.272 11.4014 11.4619C9.01622 11.9018 7.75232 10.0338 5.31963 10.0338C2.88694 10.0338 1.51855 11.4619 1.51855 11.4619V1.94125Z"
+                      fill="#FF5C01"/>
+                  <rect y="0.5" width="1.15385" height="15" fill="#FF5C01"/>
+                </svg>
+              </div>
+              <div class="section pdng-l-5px">
+                <div class=" txt-color-2 txt-size-12px txt-bold">
+                  1 337
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a class="map-mark mark" style="top:220px; left:220px;">
+          <div class="section pdng-8px">
+            <div class="flex-row flex-algn-itms-c">
+              <div class="section">
+                <svg class="block" width="22" height="14" viewBox="0 0 22 14" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path
+                      d="M11 1.25C15.0486 1.25 17.6168 2.78517 19.1763 4.28975C19.9622 5.04792 20.4986 5.80548 20.8368 6.36991C20.9913 6.62764 21.1037 6.84396 21.1792 7C21.1037 7.15604 20.9913 7.37235 20.8368 7.63009C20.4986 8.19452 19.9622 8.95208 19.1763 9.71025C17.6168 11.2148 15.0486 12.75 11 12.75C6.95137 12.75 4.38316 11.2148 2.82367 9.71025C2.03783 8.95208 1.50143 8.19452 1.16317 7.63009C1.00871 7.37235 0.89626 7.15604 0.820791 7C0.89626 6.84396 1.00871 6.62764 1.16317 6.36991C1.50143 5.80548 2.03783 5.04792 2.82367 4.28975C4.38316 2.78517 6.95137 1.25 11 1.25ZM0.700307 6.73154C0.700114 6.73104 0.70007 6.73093 0.700174 6.7312L0.700307 6.73154Z"
+                      stroke="#FF5C01" stroke-width="1.5"/>
+                  <circle cx="11" cy="6.5" r="3" stroke="#FF5C01" stroke-width="2"/>
+                </svg>
+              </div>
+              <div class="section pdng-l-5px">
+                <div class=" txt-color-1 txt-size-12px txt-bold">
+                  0
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="section pdng-8px border-l-1px border-color2">
+            <div class="flex-row flex-algn-itms-c">
+              <div class="section">
+                <svg class="block" width="14" height="16" viewBox="0 0 14 16" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path
+                      d="M1.51855 1.94125C1.51855 1.94125 3.79543 0.646169 5.31963 0.513148C7.30809 0.33961 8.96866 1.94125 10.3371 1.94125C11.7054 1.94125 13.9861 0.989181 13.9861 0.989181V10.5098C13.9861 10.5098 12.4312 11.272 11.4014 11.4619C9.01622 11.9018 7.75232 10.0338 5.31963 10.0338C2.88694 10.0338 1.51855 11.4619 1.51855 11.4619V1.94125Z"
+                      fill="#FF5C01"></path>
+                  <rect y="0.5" width="1.15385" height="15" fill="#FF5C01"></rect>
+                </svg>
+              </div>
+              <div class="section pdng-l-5px">
+                <div class=" txt-color-2 txt-size-12px txt-bold">
+                  0
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+
+      </div>
     </div>
   </div>
 </template>
 <script>
+import 'ol/ol.css'
 import Header from './Header.vue'
+import CommissionMap from './CommissionMap.vue'
 import {defineComponent, onMounted, ref} from "vue";
 
 const data = ref(null)
@@ -249,16 +261,21 @@ async function fetchCommissions() {
 export default defineComponent({
   components: {
     'header-view': Header,
+    CommissionMap
   },
   setup() {
     const view = ref('list')
+    const mapInit = ref(false)
     onMounted(() => {
       fetchCommissions()
     })
     return {
       view,
-      data
+      data,
+      mapInit
     }
   },
 })
 </script>
+<style>
+</style>
