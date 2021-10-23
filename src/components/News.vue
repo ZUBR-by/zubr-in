a99dc830304ba2fd2c80e8c1c30fc5507dedf203
Alherd <algerd.blr@gmail.com>
2021-10-23 17:47:29 +0300

    первая версия отображения списка новостей

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,13 +1,33 @@
 <template>
   <header-view></header-view>
   <div class="scene">
+    <div class="article-group" v-if="data" v-for="(items, group) in data">
+      <div class="pdng-b-40px">
+        <h3 class="txt-size-36px txt-bold">
+          {{group}}
+        </h3>
+      </div>
+      <div class="article-preview-unit shadow-type-5" v-if="data" v-for="item of items">
+        <time class="block txt-color-3-1 txt-size-14px mrgn-b-10px">
+          {{ item.date }}
+        </time>
+        <h4 class="txt-size-36px txt-bold txt-color-1">
+          <a class="txt-underline-2px" href="/news/1">
+            {{item.title.rendered}}
+          </a>
+        </h4>
+        <h5 class="txt-size-28px txt-bold mrgn-t-15px" v-html="item.excerpt.rendered">
+        </h5>
+        <div class="pdng-t-30px pdng-b-30px" v-html="item.content.rendered"></div>
+      </div>
+    </div>
     <div class="article-group">
       <div class="pdng-b-40px">
         <h3 class="txt-size-36px txt-bold">
           13 августа 2020
         </h3>
       </div>
-      <div class="article-preview-unit shadow-type-5" v-for="item of [1,2,3]">
+      <div class="article-preview-unit shadow-type-5" v-for="item of [1]">
         <time class="block txt-color-3-1 txt-size-14px mrgn-b-10px">
           19:45, сегодня
         </time>
@@ -48,12 +68,52 @@
   </div>
 </template>
 <script>
+//
 import Header from './Header.vue';
-import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
+
+function fetchNews() {
+  const data = ref(null)
+  const error = ref(null)
+  const loading = ref(false)
+  const load = async () => {
+    try {
+      loading.value = true
+      const response = await fetch('https://zubr.media/wp-json/wp/v2/posts?tags=79&per_page=3')
+      let tmp = await response.json()
+      data.value = ((items) => {
+        let result = {};
+        for (let item of items) {
+          let d = item.date.split('T')[0];
+          if (!result[d]) {
+            result[d] = [];
+          }
+          result[d].push(item)
+        }
+        return result;
+      })(tmp)
+      loading.value = false
+    } catch (e) {
+      loading.value = false
+      error.value = e.message;
+      console.error(e)
+    }
+  }
+
+  return {data, load, error, loading}
+}
 
 export default defineComponent({
   components: {
     'header-view': Header
+  },
+  setup() {
+    const {data, load, loading} = fetchNews();
+    load()
+    return {
+      data,
+      loading
+    }
   }
 })
 </script>
