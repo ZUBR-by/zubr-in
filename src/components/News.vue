96a248df586ee0859d35ec8e3270a7dfe1f37129
Alherd <algerd.blr@gmail.com>
2021-10-25 17:38:53 +0300

    Added featured image

--- src/components/News.vue
+++ src/components/News.vue
@@ -4,7 +4,7 @@
     <div class="article-group" v-if="data" v-for="(items, group) in data">
       <div class="pdng-b-40px">
         <h3 class="txt-size-36px txt-bold">
-          {{group}}
+          {{ group }}
         </h3>
       </div>
       <div class="article-preview-unit shadow-type-5" v-if="data" v-for="item of items">
@@ -18,6 +18,8 @@
         <h5 class="txt-size-28px mil-txt-size-24px txt-bold mrgn-t-15px" v-html="item.excerpt.rendered">
         </h5>
         <div class="pdng-t-30px pdng-b-30px" v-html="item.content.rendered" v-if="false"></div>
+        <img :src="item._embedded['wp:featuredmedia'][0]['source_url']"
+             v-if="item._embedded['wp:featuredmedia'][0]['source_url']">
       </div>
     </div>
     <div class="flex-column flex-algn-itms-c pdng-t-40px">
@@ -39,7 +41,7 @@ function fetchNews() {
   const load = async () => {
     try {
       loading.value = true
-      const response = await fetch('https://zubr.media/wp-json/wp/v2/posts?tags=79&per_page=10')
+      const response = await fetch('https://zubr.media/wp-json/wp/v2/posts?tags=79&per_page=10&_embed')
       let tmp = await response.json()
       data.value = ((items) => {
         let result = {};
