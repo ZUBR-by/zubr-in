6138c23eb7f204d5a3a94166cccccfded12e68f0
Alherd <algerd.blr@gmail.com>
2021-11-03 11:55:14 +0200

    Minor fixes

--- src/components/Commission.vue
+++ src/components/Commission.vue
@@ -6,17 +6,11 @@
         <div class="section">
           <div class="flex-row flex-algn-itms-c">
             <div class="section">
-              <svg class="zoom-0_75" width="25" height="33" viewBox="0 0 25 33" fill="none"
-                   xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M2.71436 2.57623C2.71436 2.57623 6.78428 0.261278 9.50878 0.0235027C13.0632 -0.286697 16.0314 2.57623 18.4774 2.57623C20.9234 2.57623 25.0001 0.874411 25.0001 0.874411V17.8926C25.0001 17.8926 22.2207 19.2549 20.3799 19.5944C16.1164 20.3807 13.8572 17.0417 9.50878 17.0417C5.16035 17.0417 2.71436 19.5944 2.71436 19.5944V2.57623Z"
-                    fill="#FF5C01"></path>
-                <rect y="1" width="2" height="32" fill="#FF5C01"></rect>
-              </svg>
+              <img src="/img/icon/flag_large.svg" class="zoom-0_75">
             </div>
             <div class="section pdng-l-15px" v-if="data">
               <div class="txt-size-18px mil-txt-size-14px">
-                Нарушений
+                Сообщений
               </div>
               <div class="txt-size-18px txt-bold mil-txt-size-14px">
                 {{ data.commission.violations.length }}
@@ -227,17 +221,11 @@
               class="section flex-algn-slf-strch flex-row flex-algn-itms-c border-l-2px border-color2 pdng-20px pdng-l-40px pdng-r-40px mil-pdng-15px mil-pdng-r-20px cursor-pointer hovered">
             <div class="flex-row flex-algn-itms-c">
               <div class="section">
-                <svg class="mil-zoom-0_65" width="25" height="33" viewBox="0 0 25 33" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                  <path
-                      d="M2.71436 2.57623C2.71436 2.57623 6.78428 0.261278 9.50878 0.0235027C13.0632 -0.286697 16.0314 2.57623 18.4774 2.57623C20.9234 2.57623 25.0001 0.874411 25.0001 0.874411V17.8926C25.0001 17.8926 22.2207 19.2549 20.3799 19.5944C16.1164 20.3807 13.8572 17.0417 9.50878 17.0417C5.16035 17.0417 2.71436 19.5944 2.71436 19.5944V2.57623Z"
-                      fill="#FF5C01"></path>
-                  <rect y="1" width="2" height="32" fill="#FF5C01"></rect>
-                </svg>
+                <img src="/img/icon/flag_large.svg" class="mil-zoom-0_65">
               </div>
               <div class="section pdng-l-15px">
                 <div class="txt-size-20px mil-txt-size-14px mil-notdisplay">
-                  Нарушений
+                  Сообщений
                 </div>
                 <div class="txt-size-20px mil-txt-size-14px txt-bold txt-nowrap">
                   1 337
