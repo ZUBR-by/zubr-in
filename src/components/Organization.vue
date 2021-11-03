f8fac2551e06d8d978e6493a8683bd14515efd49
Alherd <algerd.blr@gmail.com>
2021-11-03 15:40:05 +0200

    Inline svg

--- src/components/Organization.vue
+++ src/components/Organization.vue
@@ -5,10 +5,7 @@
         <div class="section">
           <div class="flex-row flex-algn-itms-c">
             <div class="section">
-              <svg class="mil-zoom-0_75" width="25" height="33" viewBox="0 0 25 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M2.71436 2.57623C2.71436 2.57623 6.78428 0.261278 9.50878 0.0235027C13.0632 -0.286697 16.0314 2.57623 18.4774 2.57623C20.9234 2.57623 25.0001 0.874411 25.0001 0.874411V17.8926C25.0001 17.8926 22.2207 19.2549 20.3799 19.5944C16.1164 20.3807 13.8572 17.0417 9.50878 17.0417C5.16035 17.0417 2.71436 19.5944 2.71436 19.5944V2.57623Z" fill="#FF5C01"></path>
-                <rect y="1" width="2" height="32" fill="#FF5C01"></rect>
-              </svg>
+              <img src="/img/icon/flag_large.svg" class="zoom-0_75">
             </div>
             <div class="section pdng-l-15px">
               <div class="txt-size-18px mil-txt-size-14px">
