b1677e9abbbd13bb4ec0ebaddce74e2d29ad623d
Alherd <algerd.blr@gmail.com>
2021-11-07 16:05:59 +0200

    Added info about commissions

--- src/components/Member.vue
+++ src/components/Member.vue
@@ -54,53 +54,20 @@
   </div>
   <div class="scene">
     <h2 class="txt-size-36px txt-bold pdng-b-40px mil-txt-size-30px">
-      Участие в <a class="txt-underline-2px" href="#">комиссиях</a> избирательных кампаний
+      Участие в <a class="txt-underline-2px" href="/commissions">комиссиях</a> избирательных кампаний
     </h2>
     <div class="election-campaign-list">
-      <div class="election-campaign-unit">
-        <div class="elect-camp-unit-header flex-row flex-algn-itms-c size-100 border-b-2px border-color2">
-          <div
-              class="section flex-grow-all txt-size-18px mil-txt-size-14px pdng-20px pdng-l-40px pdng-r-40px mil-pdng-15px mil-pdng-l-20px grayscale">
-            <div class="txt-color-1 txt-medium">
-              6—9 августа 2021
-            </div>
-            <div class="txt-bold">
-              Архив
-            </div>
-          </div>
-          <div
-              class="section flex-algn-slf-strch flex-row flex-algn-itms-c border-l-2px border-color2 pdng-20px pdng-l-40px pdng-r-40px mil-pdng-15px mil-pdng-r-20px cursor-pointer hovered">
-            <div class="flex-row flex-algn-itms-c">
-              <div class="section">
-                <svg class="mil-zoom-0_65" width="25" height="33" viewBox="0 0 25 33" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                  <path
-                      d="M2.71436 2.57623C2.71436 2.57623 6.78428 0.261278 9.50878 0.0235027C13.0632 -0.286697 16.0314 2.57623 18.4774 2.57623C20.9234 2.57623 25.0001 0.874411 25.0001 0.874411V17.8926C25.0001 17.8926 22.2207 19.2549 20.3799 19.5944C16.1164 20.3807 13.8572 17.0417 9.50878 17.0417C5.16035 17.0417 2.71436 19.5944 2.71436 19.5944V2.57623Z"
-                      fill="#FF5C01"></path>
-                  <rect y="1" width="2" height="32" fill="#FF5C01"></rect>
-                </svg>
-              </div>
-              <div class="section pdng-l-15px">
-                <div class="txt-size-20px mil-txt-size-14px mil-notdisplay">
-                  Нарушений
-                </div>
-                <div class="txt-size-20px mil-txt-size-14px txt-bold txt-nowrap">
-                  1 337
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div class="election-campaign-unit" v-for="item of commissions">
         <div
             class="elect-camp-unit-info pdng-t-20px pdng-b-40px pdng-l-40px pdng-r-40px mil-pdng-l-20px mil-pdng-r-20px mil-pdng-b-30px grayscale">
           <h2 class="txt-color-1 txt-size-36px mil-txt-size-30px txt-lh-1_1em">
-            <a class="txt-underline-inline" href="#">
-              Выборы представителей нижней палаты верхнего представительства третьего созыва депутатов
+            <a class="txt-underline-inline" :href="'/campaign/' + item.organization.electoral_campaign.id">
+              {{item.organization.electoral_campaign.name}}
             </a>
           </h2>
           <div class="tag-wrp mrgn-t-20px">
-            <a class="tag-unit">
-              Общереспубликанские выборы
+            <a class="tag-unit" :href="'/commission/' + item.organization.id">
+              {{item.organization.name}}
             </a>
           </div>
         </div>
@@ -162,7 +129,7 @@
       </div>
     </div>
   </div>
-  <div class="scene">
+  <div class="scene" v-if="false">
     <h2 class="txt-size-36px txt-bold pdng-b-40px mil-txt-size-30px">
       Последние инциденты в которых отмечен.
       <br>
@@ -331,11 +298,18 @@ export default defineComponent({
       const tmp = data.value.member.attachments.filter(i => i.type === 'photo_origin')
       return tmp.length > 0 ? tmp[0].url : null
     })
+    const commissions = computed(() => {
+      if (!data.value) {
+        return null
+      }
+      return data.value.member.organizations.filter(i => i.organization && i.organization.electoral_campaign)
+    })
     return {
       data,
       showModal,
       employer,
-      photoOrigin
+      photoOrigin,
+      commissions,
     }
   }
 })
