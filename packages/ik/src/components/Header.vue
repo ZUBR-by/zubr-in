81fe1b9ba2fc1c3331d26e9a40bed66413898b28
Alherd <algerd.blr@gmail.com>
2022-01-16 17:01:20 +0200

    Added new template for IK

--- packages/ik/src/components/Header.vue
+++ packages/ik/src/components/Header.vue
@@ -1,38 +1,25 @@
 <template>
-  <div class="header-wrp fixedhrd">
-    <div class="header flex-row flex-algn-itms-c">
-      <a href="/" class="section pdng-l-20px pdng-r-20px">
-        <img :src="isUik ? '/img/about/hp.png' : '/img/icon/zubr.svg'"
-             width="100"
-             height="77"
-             class="zoom-0_75 mil-zoom-0_5">
-      </a>
-      <div class="header-links flex-grow-all pdng-l-20px pdng-r-20px mil-notdisplay">
-        <router-link :to="route.to" v-for="route of routes" :class="{'active' : active === route.name}">
-          {{ route.name }}
-        </router-link>
-      </div>
-      <!-- mobile nav -->
-      <div class="section flex-grow-all pdng-l-20px pdng-r-30px notdisplay mil-show">
-        <input id="brgrbtn" class="notdisplay mil-show" type="checkbox">
-        <label for="brgrbtn" class="notdisplay burger-button mil-show">
-          <div class="burger-button-line"></div>
-          <div class="burger-button-line"></div>
-          <div class="burger-button-line"></div>
-        </label>
-        <div class="brgr-nav notdisplay mil-show">
-          <div class="header-links pdng-l-20px pdng-r-20px">
-            <div class="pdng-t-30px" v-for="route of routes">
-              <router-link :to="route.to">
-                {{ route.name }}
-              </router-link>
-            </div>
-          </div>
+    <div class="header-ik-2022 flex-row flex-algn-itms-c">
+        <div class="section pdng-20px pdng-l-5prc pdng-r-30px">
+            <a href="#">
+                <img src="/imgs/logo.svg">
+            </a>
         </div>
-      </div>
+        <div class="section pdng-r-5prc flex-grow-all txt-algn-r">
+            <a class="header-ik-2022-link txt-size-16px txt-bold active" href="#">
+                <img class="size-60" src="imgs/crpline-1.png" alt="">
+                Наблюдателям
+            </a>
+            <a class="header-ik-2022-link txt-size-16px txt-bold" href="#">
+                <img class="size-60" src="imgs/crpline-1.png" alt="">
+                Расследование фальсификаций
+            </a>
+            <a class="header-ik-2022-link txt-size-16px txt-bold" href="#">
+                <img class="size-60" src="imgs/crpline-1.png" alt="">
+                О референдуме
+            </a>
+        </div>
     </div>
-    <slot></slot>
-  </div>
 </template>
 
 <script>
@@ -40,85 +27,83 @@ import {defineComponent, ref, watch} from "vue";
 import {routes} from '../router';
 
 const hash = {
-  'members': 'Члены комиссий',
-  'organizations': 'Организации',
-  'commissions': 'Комиссии'
+    'members': 'Члены комиссий',
+    'organizations': 'Организации',
+    'commissions': 'Комиссии'
 }
 export default defineComponent({
-  props: {
-    active: String
-  },
-  components: {
-  },
-  setup(props) {
-    const query = ref();
-    const results = ref([
-      {
-        label: 'Члены комиссии',
-        items: []
-      },
-      {
-        label: 'Комиссии',
-        items: []
-      },
-      {
-        label: 'Организации',
-        items: []
-      }
-    ])
-    watch(query, (newValue) => {
-      if (!newValue) {
-        return
-      }
-      if (newValue.id && newValue.__typename === 'election_member') {
-        window.location = '/member/' + newValue.id
-        return;
-      }
-      if (newValue.id && newValue.kind) {
-        window.location = '/organization/' + newValue.id
-        return;
-      }
-      if (newValue.id) {
-        window.location = '/commission/' + newValue.id
-      }
-    })
-    return {
-      routes: routes.filter(i => !i.exclude).map(item => {
-        return {
-          to: item.path,
-          name: item.meta.title
-        }
-      }),
-      results,
-      query,
-      isUik,
-      active: props.active,
-      async load({value}) {
-        try {
-          const response = await fetch(
-              import.meta.env.VITE_API_URL
-              + '/search'
-              + (value ? '?name=' + encodeURIComponent('%' + value + '%') : '')
-          )
-          const payload = await response.json();
-          ((tmp) => {
-            let final = [];
-            for (let key in tmp) {
-              if (tmp[key].length === 0) {
-                continue;
-              }
-              final.push({
-                'label': hash[key],
-                items: tmp[key]
-              })
+    props: {
+        active: String
+    },
+    components: {},
+    setup(props) {
+        const query   = ref();
+        const results = ref([
+            {
+                label: 'Члены комиссии',
+                items: []
+            },
+            {
+                label: 'Комиссии',
+                items: []
+            },
+            {
+                label: 'Организации',
+                items: []
             }
-            results.value = final
-          })(payload)
-        } catch (e) {
-          console.error(e)
+        ])
+        watch(query, (newValue) => {
+            if (!newValue) {
+                return
+            }
+            if (newValue.id && newValue.__typename === 'election_member') {
+                window.location = '/member/' + newValue.id
+                return;
+            }
+            if (newValue.id && newValue.kind) {
+                window.location = '/organization/' + newValue.id
+                return;
+            }
+            if (newValue.id) {
+                window.location = '/commission/' + newValue.id
+            }
+        })
+        return {
+            routes: routes.filter(i => !i.exclude).map(item => {
+                return {
+                    to: item.path,
+                    name: item.meta.title
+                }
+            }),
+            results,
+            query,
+            active: props.active,
+            async load({value}) {
+                try {
+                    const response = await fetch(
+                        import.meta.env.VITE_API_URL
+                        + '/search'
+                        + (value ? '?name=' + encodeURIComponent('%' + value + '%') : '')
+                    )
+                    const payload  = await response.json();
+                    ((tmp) => {
+                        let final = [];
+                        for (let key in tmp) {
+                            if (tmp[key].length === 0) {
+                                continue;
+                            }
+                            final.push({
+                                'label': hash[key],
+                                items: tmp[key]
+                            })
+                        }
+                        results.value = final
+                    })(payload)
+                } catch (e) {
+                    console.error(e)
+                }
+            }
         }
-      }
     }
-  }
 });
 </script>
