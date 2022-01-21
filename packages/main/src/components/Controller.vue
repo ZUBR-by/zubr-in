9a6977d34aa65a5bca95fd223e530adc0a643b6e
m__d <i@artemnaumov.by>
2022-01-21 12:14:12 +0300

    Controller update

--- packages/main/src/components/Controller.vue
+++ packages/main/src/components/Controller.vue
@@ -1,22 +1,24 @@
 <template>
     <header-view></header-view>
     <div class="scene mrgn-t-20px mil-mrgn-t-80px">
-        <div class="section">
-            <img src="/img/controller/become.png" width="650" height="340">
+        <div class="section size-100">
+            <img class="size-50 mil-size-100" src="/img/controller/become.png">
         </div>
-        <a class="button primary large flex-row flex-algn-itms-c size-25 mil-size-25" :href='botURL'>
+        <div class="section size-100 mrgn-t-30px">
+          <a class="button primary large inline-flex flex-algn-itms-c" :href='botURL'>
             <div class="section">
-                Присоединиться
+              Присоединиться
             </div>
             <div class="section mrgn-l-10px">
-                <svg class="zoom-1_2" width="14" height="12" viewBox="0 0 14 12" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M1 5.25C0.585786 5.25 0.25 5.58579 0.25 6C0.25 6.41421 0.585786 6.75 1 6.75V5.25ZM13.5303 6.53033C13.8232 6.23744 13.8232 5.76256 13.5303 5.46967L8.75736 0.696699C8.46447 0.403806 7.98959 0.403806 7.6967 0.696699C7.40381 0.989593 7.40381 1.46447 7.6967 1.75736L11.9393 6L7.6967 10.2426C7.40381 10.5355 7.40381 11.0104 7.6967 11.3033C7.98959 11.5962 8.46447 11.5962 8.75736 11.3033L13.5303 6.53033ZM1 6.75H13V5.25H1V6.75Z"
-                        fill="white"></path>
-                </svg>
+              <svg class="zoom-1_2" width="14" height="12" viewBox="0 0 14 12" fill="none"
+                   xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M1 5.25C0.585786 5.25 0.25 5.58579 0.25 6C0.25 6.41421 0.585786 6.75 1 6.75V5.25ZM13.5303 6.53033C13.8232 6.23744 13.8232 5.76256 13.5303 5.46967L8.75736 0.696699C8.46447 0.403806 7.98959 0.403806 7.6967 0.696699C7.40381 0.989593 7.40381 1.46447 7.6967 1.75736L11.9393 6L7.6967 10.2426C7.40381 10.5355 7.40381 11.0104 7.6967 11.3033C7.98959 11.5962 8.46447 11.5962 8.75736 11.3033L13.5303 6.53033ZM1 6.75H13V5.25H1V6.75Z"
+                    fill="white"></path>
+              </svg>
             </div>
-        </a>
+          </a>
+        </div>
     </div>
     <div class="scene pdng-t-5px mil-pdng-b-5px">
         <div class="flex-row mil-flex-wrap">
@@ -39,7 +41,7 @@
                 </p>
             </div>
             <div class="section size-50 mil-size-100 mil-pdng-0">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/TI-rcwBgxCE"
+                <iframe class="mil-size-100" width="560" height="315" src="https://www.youtube.com/embed/TI-rcwBgxCE"
                         title="YouTube video player"
                         frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
