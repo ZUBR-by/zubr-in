5a2766a673f3f4f6a58492e3730400ecfd912fc7
Alherd <algerd.blr@gmail.com>
2022-01-23 23:00:22 +0200

    Fixed header

--- packages/ik/src/components/Header.vue
+++ packages/ik/src/components/Header.vue
@@ -19,6 +19,28 @@
                 О референдуме
             </a>
         </div>
+        <div class="section flex-grow-all pdng-l-20px pdng-r-30px notdisplay mil-show">
+            <input id="brgrbtn" class="notdisplay mil-show" type="checkbox">
+            <label for="brgrbtn" class="notdisplay burger-button mil-show">
+                <div class="burger-button-line"></div>
+                <div class="burger-button-line"></div>
+                <div class="burger-button-line"></div>
+            </label>
+            <div class="brgr-nav notdisplay mil-show">
+                <a class="header-ik-2022-link txt-size-16px txt-bold active" href="https://zubr.in/controller">
+                    <img class="size-60" src="imgs/crpline-1.png" alt="">
+                    Наблюдение и гражд. контроль
+                </a>
+                <router-link  class="header-ik-2022-link txt-size-16px txt-bold" :to="{name: 'schema'}">
+                    <img class="size-60" src="imgs/crpline-1.png" alt="">
+                    Расследование фальсификаций
+                </router-link>
+                <a class="header-ik-2022-link txt-size-16px txt-bold" href="#">
+                    <img class="size-60" src="imgs/crpline-1.png" alt="">
+                    О референдуме
+                </a>
+            </div>
+        </div>
     </div>
 </template>
 
