f04e872b311b677e05ae77454a3d6d26f00edd4e
Alherd <algerd.blr@gmail.com>
2022-01-30 19:21:08 +0200

    Added links

--- packages/ik/src/components/Header.vue
+++ packages/ik/src/components/Header.vue
@@ -534,6 +534,11 @@
                 <img class="size-60" src="/imgs/crpline-1.png" alt="">
                 Наблюдателям
             </a>
+            <router-link class="header-ik-2022-link txt-size-16px txt-bold txt-ik2022-hdr"
+                         :to="{name: 'to_honest_members'}">
+                <img class="size-60" src="/imgs/crpline-1.png" alt="">
+                Членам комиссий
+            </router-link>
             <router-link class="header-ik-2022-link txt-size-16px txt-bold txt-ik2022-hdr" :to="{name: 'schema'}">
                 <img class="size-60" src="/imgs/crpline-1.png" alt="">
                 Расследование фальсификаций
@@ -555,6 +560,10 @@
                     <img class="size-60" src="/imgs/crpline-1.png" alt="">
                     Наблюдателям
                 </a>
+                <router-link class="header-ik-2022-link txt-size-16px txt-bold" :to="{name: 'to_honest_members'}">
+                    <img class="size-60" src="/imgs/crpline-1.png" alt="">
+                    Членам комиссий
+                </router-link>
                 <router-link class="header-ik-2022-link txt-size-16px txt-bold" :to="{name: 'schema'}">
                     <img class="size-60" src="/imgs/crpline-1.png" alt="">
                     Расследование фальсификаций
--- packages/ik/src/components/ToHonestMembers.vue
+++ packages/ik/src/components/ToHonestMembers.vue
@@ -1,10 +1,14 @@
 <template>
-
+    <header-view>
+    </header-view>
 </template>
 <script>
+import Header from './Header.vue';
 import {defineComponent} from "vue";
 
 export default defineComponent({
-
+    components: {
+        'header-view': Header,
+    }
 })
 </script>
